c548f183f55254d07e5407051466e314383953d0
Rkulkrni <rahulspk7@gmail.com>
2025-11-22 21:18:31 +0530

    app routing file updated.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path:'', loadChildren:()=> import('../modules/product-list/product-list.module').then(m=>m.ProductListModule)},
+    { path:'', loadChildren:()=> import('../modules/product-list/product.routes').then(m=>m.adminRoutes)},
     { path:'shared', loadChildren:()=> import('../modules/shared/shared.module').then(m=>m.SharedModule)},
-    { path:'admin', loadChildren:()=> import('../modules/admin/admin.module').then(m=>m.AdminModule)}
+    { path:'admin', loadChildren:()=>  import('../modules/admin/admin.routes').then(m => m.adminRoutes)}
 ];
