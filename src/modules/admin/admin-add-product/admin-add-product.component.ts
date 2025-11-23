81d8b533e48d9f514f59d09d1301860edfb69796
Rkulkrni <rahulspk7@gmail.com>
2025-11-23 16:29:21 +0530

    bugfix POST api

--- src/modules/admin/admin-add-product/admin-add-product.component.ts
+++ src/modules/admin/admin-add-product/admin-add-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { ProductService } from '../../../services/product.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-admin-add-product',
@@ -8,33 +9,50 @@ import { ProductService } from '../../../services/product.service';
   standalone: true,
   templateUrl: './admin-add-product.component.html',
   styleUrl: './admin-add-product.component.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AdminAddProductComponent {
-  addProductForm:any;
+  addProductForm: any;
   isShowNotification = signal<boolean>(false);
-  constructor(private _fb:FormBuilder, private _productService:ProductService) {
+  constructor(
+    private _fb: FormBuilder,
+    private _productService: ProductService,
+    private _toastr: ToastrService
+  ) {
     this.addProductForm = this._fb.group({
-      product_name:[''],
-      product_decription:[''],
-      purchasePrice:[''],
-      sellingPrice:[''],
-      quantity:[''],
-      image_url:[''],
-      status:[''],
-      createdDate:[''],
-      created_by:[''],
-      modifyDate:[''],
-      modified_by:['']
+      product_name: [''],
+      product_decription: [''],
+      purchasePrice: [''],
+      sellingPrice: [''],
+      quantity: [''],
+      image_url: [''],
+      status: [''],
+      createdDate: [''],
+      created_by: [''],
+      modifyDate: [''],
+      modified_by: [''],
     });
   }
 
   onSaveClick() {
     const payload = this.addProductForm.value;
-    this._productService.saveProduct(payload).subscribe((response)=>{
-      this.isShowNotification.set(true);
+    this._productService.saveProduct(payload).subscribe({
+      next: (response: any) => {
+        this.addProduct(response);
+        // this._productService.getProductList();
+        this._productService.SubscribeGetProductList();
+        this._toastr.success('Product saved successfully!');
+      },
+      error: (err: any) => {
+        console.error('Failed to add product:', err);
+        this._toastr.error('Failed to add product!');
+      },
     });
-    console.log(this.addProductForm.value);
+  }
+
+  // add product
+  addProduct(product: any) {
+    this._productService.products.update((old) => [...old, product]);
   }
 
   closeNotification() {
