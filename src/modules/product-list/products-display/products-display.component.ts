c9cafa1f2c84fe47f9ce260733e428d8da2caa22
Rkulkrni <rahulspk7@gmail.com>
2025-11-23 22:19:52 +0530

    Product displayed in cards

--- src/modules/product-list/products-display/products-display.component.ts
+++ src/modules/product-list/products-display/products-display.component.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductService } from '../../../services/product.service';
 @Component({
   selector: 'app-products-display',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, CommonModule],
   templateUrl: './products-display.component.html',
   styleUrl: './products-display.component.css'
 })
 export class ProductsDisplayComponent {
+  
+  allProductList = signal<any>([]);
+  constructor(private _productService:ProductService) {}
+  ngOnInit() {
+    this.allProductList = this._productService.SubscribeGetProductList();
+  }
 
+  addToCart(proudct:any) {}
 }
