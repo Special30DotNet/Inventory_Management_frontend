f0bdc98522cc52525150fd47c3f8fea992134313
Rkulkrni <rahulspk7@gmail.com>
2025-11-29 20:06:09 +0530

    login form created in frontend

--- src/modules/product-list/products-display/products-display.component.ts
+++ src/modules/product-list/products-display/products-display.component.ts
@@ -1,20 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductService } from '../../../services/product.service';
+import { ɵInternalFormsSharedModule, ReactiveFormsModule, FormBuilder } from "@angular/forms";
 @Component({
   selector: 'app-products-display',
-  imports: [CommonModule, RouterModule, CommonModule],
+  imports: [CommonModule, RouterModule, CommonModule, ɵInternalFormsSharedModule, ReactiveFormsModule],
   templateUrl: './products-display.component.html',
-  styleUrl: './products-display.component.css'
+  styleUrl: './products-display.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ProductsDisplayComponent {
   
   allProductList = signal<any>([]);
-  constructor(private _productService:ProductService) {}
+  loginForm : any;
+  constructor(private _productService:ProductService, private _fb:FormBuilder) {
+    this.loginForm = this._fb.group({
+      userName : [''],
+      password : ['']
+    });
+  }
   ngOnInit() {
     this.allProductList = this._productService.SubscribeGetProductList();
   }
 
   addToCart(proudct:any) {}
+
+  onClickLogin() {
+
+  }
 }
