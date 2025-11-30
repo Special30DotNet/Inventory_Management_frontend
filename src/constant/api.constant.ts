afb3d4bf7518acab06026f85a5a8717ee76d05b7
Rkulkrni <rahulspk7@gmail.com>
2025-11-30 11:33:24 +0530

    basic login UI implemented.

--- src/constant/api.constant.ts
+++ src/constant/api.constant.ts
@@ -1,5 +1,10 @@
 const baseUrl = 'https://localhost:7159/api/'
+
+// Product api's
 export const getProductList = baseUrl + 'Products';
 export const addProductToStore = baseUrl + 'AddProduct';
 export const deleteProduct = baseUrl + 'DeleteRecord/';
 export const editProduct = '';
+
+// Admin api's
+export const loginUrl = baseUrl + 'adminLogin';
--- src/modules/product-list/products-display/products-display.component.ts
+++ src/modules/product-list/products-display/products-display.component.ts
@@ -1,32 +1,56 @@
 import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { ProductService } from '../../../services/product.service';
 import { ɵInternalFormsSharedModule, ReactiveFormsModule, FormBuilder } from "@angular/forms";
+import { UtilityService } from '../../../services/utility.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-products-display',
-  imports: [CommonModule, RouterModule, CommonModule, ɵInternalFormsSharedModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    CommonModule,
+    ɵInternalFormsSharedModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './products-display.component.html',
   styleUrl: './products-display.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ProductsDisplayComponent {
-  
   allProductList = signal<any>([]);
-  loginForm : any;
-  constructor(private _productService:ProductService, private _fb:FormBuilder) {
+  loginForm: any;
+  constructor(
+    private _productService: ProductService,
+    private _fb: FormBuilder,
+    private _loginService: UtilityService,
+    private _router: Router,
+    private _toaster: ToastrService
+  ) {
     this.loginForm = this._fb.group({
-      userName : [''],
-      password : ['']
+      userName: [''],
+      password: [''],
     });
   }
   ngOnInit() {
     this.allProductList = this._productService.SubscribeGetProductList();
   }
 
-  addToCart(proudct:any) {}
+  addToCart(proudct: any) {}
 
-  onClickLogin() {
-
+  onClickLogin(loginForm:any) {
+    const reqBody = loginForm.value;
+    this._loginService.loginAdmin(reqBody).subscribe({
+      next: (response: any) => {
+        if (response?.isUserVerified) {
+          this._router.navigate(['admin']);
+          this._toaster.success(response?.message);
+        }
+      },
+      error:(err:any)=> {
+        this._toaster.error(err);
+      },
+    });
   }
 }
