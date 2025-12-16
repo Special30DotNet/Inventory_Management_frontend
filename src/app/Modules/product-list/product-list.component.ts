5c39da6c5b0d87a7c17f3fb520a824358f979c82
sagarmasal12 <sagarmasal12@gmail.com>
2025-12-16 11:26:00 +0530

    Implement Supplier Management with CRUD operations and dashboard counts

--- src/app/Modules/product-list/product-list.component.ts
+++ src/app/Modules/product-list/product-list.component.ts
@@ -1,11 +1,158 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductServiceService } from '../../Core/Service/product-service.service';
+import { CommonModule, DatePipe } from '@angular/common';
+import { Product } from '../../Core/Model/productModel';
+import { Observable } from 'rxjs';
+declare var bootstrap: any;
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [CommonModule, DatePipe, ReactiveFormsModule],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  productForm: FormGroup;
+  products: any[] = [];
+  editId: null | number = null;
 
+  Counts: any = {
+    productCount: 0,
+    lowstock: 0,
+    outofstock: 0,
+    isActive: false,
+  };
+
+  constructor(private prodSrv: ProductServiceService) {
+    this.productForm = new FormGroup({
+      product_name: new FormControl('', Validators.required),
+      purchase_price: new FormControl('', Validators.required),
+      product_selling_price: new FormControl('', Validators.required),
+      product_stock_quantity: new FormControl('', Validators.required),
+      status: new FormControl('Available', Validators.required),
+      product_decription: new FormControl(''),
+      created_at: new FormControl(''),
+      created_by: new FormControl(''),
+      modified_at: new FormControl(''),
+      modified_by: new FormControl(''),
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadProducts();
+    this.dashboardCounts();
+  }
+
+  loadProducts() {
+    this.prodSrv.getallProducts().subscribe((res: any) => {
+      this.products = res;
+      console.log(this.products);
+    });
+  }
+
+  dashboardCounts() {
+    this.prodSrv.getdashboardCount().subscribe((res: any) => {
+      // console.log("Dashboard Count:- ",res)
+      this.Counts = res;
+      console.log(this.Counts);
+    });
+  }
+
+  addProducts() {
+    if (this.productForm.invalid) {
+      this.productForm.markAllAsTouched();
+      return;
+    }
+
+    const model: Product = this.productForm.value;
+    model.created_at = new Date().toISOString();
+    model.created_by = 'Owner';
+
+    this.prodSrv.addProduct(model).subscribe({
+      next: (res) => {
+        this.productForm.reset()
+        this.loadProducts();
+        console.log('Product Add:', res);
+        alert('Prouct Added Successfully');
+
+      },
+      error: (err) => {
+        console.error('Error Add', err);
+        alert('something went to wrong while adding product');
+      },
+    });
+    console.log(this.productForm.value);
+  }
+
+  updateProduct(item: any) {
+    this.editId = item.id; // example
+    this.productForm.patchValue({
+      product_name: item.product_name,
+      purchase_price: item.purchase_price,
+      product_selling_price: item.product_selling_price,
+      product_stock_quantity: item.product_stock_quantity,
+      product_decription: item.product_decription,
+      status: item.status,
+      created_at: item.created_at,
+      created_by: item.created_by,
+      modified_at: item.modified_at,
+      modified_by: item.modified_by,
+    });
+
+    
+    const modalElement = document.getElementById('editModal');
+    // const modalInstance = bootstrap.Modal.getInstance(modalElement);
+    const modalInstance = new bootstrap.Modal(modalElement);
+    modalInstance.show();
+  }
+
+  saveUpdate() {
+    if (this.editId !== null && this.productForm.valid) {
+      const updatedProduct = this.productForm.value;
+      this.prodSrv.updateprod(this.editId, updatedProduct).subscribe({
+        next: (res) => {
+          console.log('Product updated', res);
+          this.loadProducts();
+          this.productForm.reset({ status: '1' });
+          this.editId = null;
+
+          // Close modal
+          const modalElemen = document.getElementById('editModal');
+          const modalInstance = bootstrap.Modal.getInstance(modalElemen);
+          modalInstance.hide();
+        },
+        error: (err) => console.error(err),
+      });
+    } else {
+      console.error('Edit ID is null or form invalid');
+    }
+  }
+  openAddModal() {
+    this.editId = null;
+    this.productForm.reset({ status: 'Available' });
+
+    const modalElement = document.getElementById('editModal');
+    const modalInstance = new bootstrap.Modal(modalElement);
+    modalInstance.show();
+  }
+
+  deleteProduct(id:number){
+    this.prodSrv.deleteProd(id).subscribe({
+      next:(res)=>{
+        console.log("Deleted product",res)
+        alert("Product id deleted successfully")
+        this.loadProducts();
+      },
+      error:(err)=>{
+        console.error("DeleteError",err)
+        alert("Error while deleting product")
+      }
+    })
+  }
 }
--- src/app/Modules/supplier-list/supplier-list.component.ts
+++ src/app/Modules/supplier-list/supplier-list.component.ts
@@ -1,11 +1,106 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SupplierServiceService } from './supplier-service.service';
+import { CommonModule } from '@angular/common';
+import { Validators, ɵInternalFormsSharedModule, ReactiveFormsModule, FormGroup, FormBuilder, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-supplier-list',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule,FormsModule],
   templateUrl: './supplier-list.component.html',
   styleUrl: './supplier-list.component.css'
 })
-export class SupplierListComponent {
+export class SupplierListComponent implements OnInit {
+
+  supplierList:any[]=[];
+  searchText:string="";
+
+  isDatanotFound:boolean = false;
+  Counts: any = {
+    totalsupplier: 0,
+    totalActive: 0,
+    totalInactive: 0,
+   
+  };
+  supplierForm!: FormGroup;
+  
+
+  constructor(private suppSrv:SupplierServiceService,private fb:FormBuilder){
+     this.supplierForm = this.fb.group({
+      supplier_name: ['', Validators.required], // Required field
+      contactPerson: [''],
+      phoneno: [''],
+      email: [''],
+      gstumber: [''],
+      status: ['Active'],                        // Default Active
+      address: [''],
+      note: ['']
+    });
+
+        this.getall();
+
+  }
+
+
+  ngOnInit() {
+    this.getSupplierCount();
+  }
+
+  getall(){
+    this.suppSrv.getAll().subscribe((res:any)=>{
+      this.supplierList= res;
+    
+      console.log("supplierlist:",res)
+    })
+  }
+  getSupplierCount(){
+   this.suppSrv.getSupplierCount().subscribe((res:any)=>{
+    this.Counts = res;
+    console.log("Supplier Service:",res)
+   })
+  }
+
+  onSaveSupplier():void{
+    if (this.supplierForm.invalid){
+      alert('Please fill required fields')
+      return
+    }
+    this.suppSrv.addSupplier(this.supplierForm.value).subscribe({
+      next:(res)=>{
+        alert('Supplier added successfully');
+        this.supplierList.push(res);
+      },
+      error:(err)=>{
+        console.error(err)
+        alert("Error while saving supplier")
+      }
+    })
+  }
+
+  OnSearch(){
+      // Avoid empty API call
+    if (this.searchText.trim() === '') {
+      this.getall();
+      this.supplierList =[];
+      
+      return;
+    }
+
+    this.suppSrv.searchSupplier(this.searchText).subscribe({
+      next:(res)=>{
+        this.supplierList = res;
+        this.isDatanotFound = res.length==0;
+
+        console.log("search supplierName:- ",res)
+      },
+      error:(err)=>{
+        console.error("Search Error:-",err)
+        this.isDatanotFound = false;
+      }
+    })
+  }
+
+  
+
+
 
 }
